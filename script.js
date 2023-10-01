323982e84222e2f527deb96f7059f596dc77608d
nilanjan1101 <nilanjan.mech@gmail.com>
2023-10-01 22:39:51 +0530

    navbar style raedy for desktop and 900px

---
+++ script.js
@@ -0,0 +1,27 @@
+// ----------- NAVIGATION BAR FUNCTION -----------
+function myMenuFunction() {
+  var menuBtn = document.getElementById("myNavMenu");
+  if (menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
+
+// ----------- ADD SHADOW NAVIGATION BAR WHILE SCROLLING -----------
+
+// ----------- TYPING EFFECT -----------
+
+// --------- ## -- SCROL REVEAL ANIMATION -- ## ---------
+
+// ----------- HOME -----------
+
+// ----------- PROJECT BOX -----------
+
+// ----------- HEADINGS -----------
+
+// ----------- HOME -----------
+
+// ----------- HOME -----------
+
+// ----------- HOME -----------
